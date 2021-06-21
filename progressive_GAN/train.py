fd1451e894d52343b795d4c1ffe0ea532aa9ab6c
gcpage <griffinpage9@gmail.com>
2021-06-21 14:04:50 -0400

    Reimplemented training loop

--- progressive_GAN/train.py
+++ progressive_GAN/train.py
@@ -8,7 +8,9 @@ from model import *
 
 root_dir = ''
 
+
 def load_real_samples(filename):
+
     data = np.load(filename)
     X = data['arr_0']
     X = X.astype('float32')
@@ -18,6 +20,7 @@ def load_real_samples(filename):
 
 
 def generate_real_samples(dataset, n_samples):
+
     ix = np.random.randint(0, dataset.shape[0], n_samples)
     X = dataset[ix]
     y = np.ones((n_samples, 1))
@@ -26,6 +29,7 @@ def generate_real_samples(dataset, n_samples):
 
 
 def generate_latent_points(latent_dim, n_samples):
+
     x_input = np.random.randn(latent_dim * n_samples)
     x_input = x_input.reshape(n_samples, latent_dim)
 
@@ -33,6 +37,7 @@ def generate_latent_points(latent_dim, n_samples):
 
 
 def generate_fake_samples(generator, latent_dim, n_samples):
+
     x_input = generate_latent_points(latent_dim, n_samples)
     X = generator.predict(x_input)
     y = -np.ones((n_samples, 1))
@@ -41,6 +46,7 @@ def generate_fake_samples(generator, latent_dim, n_samples):
 
 
 def update_fadein(models, step, n_steps):
+
     alpha = step / float(n_steps - 1)
     for model in models:
         for layer in model.layers:
@@ -55,8 +61,9 @@ class TrainingSession(object):
                  latent_size=100,
                  n_fmap=256,
                  n_blocks=6,
-                 block_batch_sizes=[],
-                 block_steps=[]):
+                 block_batch_sizes=(),
+                 block_steps=(),
+                 data_path='data/vfp_256.npz'):
 
         self.session_id = session_id
 
@@ -68,25 +75,29 @@ class TrainingSession(object):
                 self.config = json.load(config_file)
         else:
             self.config = {'id': self.session_id,
-                           'phase': 0,
+                           'block': 0,
                            'steps': 0,
+                           'model_version': 0,
                            'latent_size': latent_size,
                            'n_fmap': n_fmap,
                            'n_blocks': n_blocks,
-                           'block_batch_sizes':block_batch_sizes,
-                           'block_steps':block_steps}
+                           'block_batch_sizes': list(block_batch_sizes),
+                           'block_steps': list(block_steps),
+                           'data_path': data_path}
 
-        self.phase = self.config['phase']
+        self.block = self.config['block']
         self.steps = self.config['steps']
+        self.model_version = self.config['model_version']
         self.block_batch_sizes = self.config['block_batch_sizes']
         self.block_steps = self.config['block_steps']
+        self.data_path = self.config['data_path']
 
         self.gan = ProgressiveGAN(latent_size=self.config['latent_size'],
                                   n_fmap=self.config['n_fmap'],
                                   n_blocks=self.config['n_blocks'],
                                   input_shape=self.config['input_shape'])
 
-        if self.phase == 0 and self.steps == 0:
+        if self.block == 0 and self.steps == 0:
             self.gen_models = self.gan.build_gen()
             self.dis_models = self.gan.build_dis()
             self.comp_models = self.gan.build_composite()
@@ -94,111 +105,86 @@ class TrainingSession(object):
             # TODO: load models
             pass
 
-        self.scaled_data =
+        self.dataset = load_real_samples(data_path)
+        self.scaled_dataset = None
+        self.scale_dataset()
 
-    def train_step(self):
+    def scale_dataset(self):
 
-        b = np.floor((self.phase + 1) / 2.0)  # Block index
-        m = self.phase % 2  # Model version (0:straight-through, 1:fade-in)
+        images_list = list()
+        res = 2 ** (2 + self.block)
+        for image in self.dataset:
+            new_image = np.resize(image, [res, res, 3])
+            images_list.append(new_image)
+        self.scaled_dataset = np.asarray(images_list)
 
-        g_model = self.gen_models[b][m]
-        d_model = self.dis_models[b][m]
-        comp_model = self.comp_models[b][m]
+    def next_block(self):
 
-        # Update alpha
-        if m == 1:
-            update_fadein([g_model, d_model, comp_model], i, n_steps)
+        self.block += 1
+        self.model_version = 1
+        self.scale_dataset()
+
+    def train(self):
+
+        if self.steps > self.block_steps[self.block]:
+            if self.block == 0 or self.model_version == 0:
+                self.next_block()
+            elif self.block == self.gan.n_blocks - 1:
+                pass
+            else:
+                self.model_version = 0
+
+        g_model = self.gen_models[self.block][self.model_version]
+        d_model = self.dis_models[self.block][self.model_version]
+        comp_model = self.comp_models[self.block][self.model_version]
+
+        batch_size = self.block_batch_sizes[self.block]
+
+        # Update alpha if needed
+        if self.model_version == 1:
+            update_fadein([g_model, d_model, comp_model], self.steps, self.block_steps[self.block])
 
         # Prepare samples
-        X_real, y_real = generate_real_samples(dataset, half_batch)
-        X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+        X_real, y_real = generate_real_samples(self.scaled_dataset, batch_size / 2)
+        X_fake, y_fake = generate_fake_samples(g_model, self.gan.latent_size, batch_size / 2)
 
         # Train discriminator
         d_loss1 = d_model.train_on_batch(X_real, y_real)
         d_loss2 = d_model.train_on_batch(X_fake, y_fake)
 
         # Train generator
-        z_input = generate_latent_points(latent_dim, n_batch)
-        y_real2 = np.ones((n_batch, 1))
+        z_input = generate_latent_points(self.gan.latent_size, batch_size)
+        y_real2 = np.ones((batch_size, 1))
         g_loss = comp_model.train_on_batch(z_input, y_real2)
 
-        return d_loss1, d_loss2, g_loss
+        if self.steps % 100 == 0:
+            print('\nStep ' + str(self.steps) + ':')
+            print('D_real:', d_loss1)
+            print('D_fake:', d_loss2)
+            print('G:', g_loss)
 
-    def train(self):
+        if self.steps % 500 == 0:
+            self.save()
 
+        if self.steps % 1000 == 0:
+            self.evaluate()
 
+    def save(self):
+        # TODO: implement
+        pass
 
-    def train_old(self, g_models, d_models, gan_models, dataset, latent_dim, e_norm, e_fadein, n_batch):
+    def evaluate(self):
+        # TODO: implement
+        pass
 
-        g_normal, d_normal, gan_normal = g_models[0][0], d_models[0][0], gan_models[0][0]
+    def is_complete(self):
 
-        # Scale dataset for base level
-        gen_shape = g_normal.output_shape
-        scaled_data = scale_dataset(dataset, gen_shape[1:])
-        print('Scaled Data', scaled_data.shape)
-
-        # Train base model
-        train_epochs_old(g_normal, d_normal, gan_normal, scaled_data, e_norm[0], n_batch[0])
-        summarize_performance('tuned', g_normal, latent_dim)
-
-        # Train each model level
-        for i in range(1, len(g_models)):
-            # Get level models
-            [g_normal, g_fadein] = g_models[i]
-            [d_normal, d_fadein] = d_models[i]
-            [gan_normal, gan_fadein] = gan_models[i]
-
-            # Scale dataset for current level
-            gen_shape = g_normal.output_shape
-            scaled_data = scale_dataset(dataset, gen_shape[1:])
-            print('Scaled Data', scaled_data.shape)
-
-            # Train transition model for next level
-            train_epochs(g_fadein, d_fadein, gan_fadein, scaled_data, e_fadein[i], n_batch[i], True)
-            summarize_performance('faded', g_fadein, latent_dim)
-
-            # Train straight-through model
-            train_epochs(g_normal, d_normal, gan_normal, scaled_data, e_norm[i], n_batch[i])
-            summarize_performance('tuned', g_normal, latent_dim)
+        return self.block == self.gan.n_blocks - 1 \
+               and self.model_version == 0 \
+               and self.steps > self.block_steps[self.block]
 
 
-def scale_dataset(images, new_shape):
-    images_list = list()
-    for image in images:
-        new_image = np.resize(image, new_shape)
-        images_list.append(new_image)
-    return np.asarray(images_list)
-
-
-def summarize_performance(status, g_model, latent_dim, n_samples=25):
-    # Naming
-    gen_shape = g_model.output_shape
-    name = '%03dx%03d-%s' % (gen_shape[1], gen_shape[2], status)
-
-    # Generate images
-    X, _ = generate_fake_samples(g_model, latent_dim, n_samples)
-    X = (X - X.min()) / (X.max() - X.min())
-    square = int(np.sqrt(n_samples))
-    for i in range(n_samples):
-        plt.subplot(square, square, 1 + i)
-        plt.axis('off')
-        plt.imshow(X[i])
-
-    # Save images
-    filename1 = 'plot_%s.png' % name
-    plt.savefig(root_dir + 'results/' + filename1)
-    plt.close()
-
-    # Save generator model
-    filename2 = 'model_%s' % name
-    save_model(g_model, filename2, gen_shape[1], 'pg1')
-
-    print('>Saved: %s and %s' % (filename1, filename2))
-
-
-
-
-
+'''
 if __name__ == '__main__':
 
     n_blocks = 6  # (2^2..2^(n+1))
@@ -219,3 +205,4 @@ if __name__ == '__main__':
     n_batch = [16, 16, 16, 8, 4, 4]
     n_epochs = [5, 8, 8, 10, 10, 10]
     train(g_models, d_models, gan_models, dataset, latent_dim, n_epochs, n_epochs, n_batch)
+'''

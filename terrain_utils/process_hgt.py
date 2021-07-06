fixed normalization of semantic representations

terrain_utils/process_hgt.py
@@ -51,14 +51,13 @@ def hgt_to_jpg(file, target, x, y, image=True):
         pos = np.random.randint(0, d - sample_size, 2)
         sample = np.copy(hgt[pos[0]:pos[0] + sample_size, pos[1]:pos[1] + sample_size])
         sample = np.rot90(sample, k=np.random.randint(0, 4), axes=[0, 1])
-        mean = np.mean(sample) / max_elevation
         s_min = np.amin(sample)
         s_max = np.amax(sample)
-        scale = (s_max - s_min) / max_elevation
         sample = (sample - s_min) / (s_max - s_min)
         sample = np.expand_dims(sample, axis=-1)
         x.append(sample)
-        y.append([mean, scale])
+        y.append([s_min / max_elevation, s_max / max_elevation])
+
         if image:
             rgb = np.zeros(shape=[sample_size, sample_size, 3])
             rgb[:, :, 0] = sample[:, :, 0]
@@ -87,7 +86,7 @@ data_archive = 'data/vfp_256_labeled.npz'
 
 x = []
 y = []
-process_all(raw_dir, img_dir, x, y, True)
+process_all(raw_dir, img_dir, x, y, False)
 x = np.asarray(x)
 y = np.asarray(y)
 print(x.shape, y.shape)

terrain_utils/semantic_regression.py
@@ -6,16 +6,18 @@ import numpy as np
 data_path = 'data/vfp_256_labeled.npz'
 
 
-def build_model(n_conv_layers, n_fmap, input_shape,
+def build_model(features, n_conv_layers, n_fmap, input_shape,
                 padding='same',
                 activation='relu',
                 dropout=0.1):
 
     model = Sequential()
     model.add(Conv2D(n_fmap[0], kernel_size=3, padding=padding, activation=activation, input_shape=input_shape))
+    model.add(Conv2D(n_fmap[0], kernel_size=3, padding=padding, activation=activation, input_shape=input_shape))
     model.add(MaxPooling2D())
 
     for i in range(1, n_conv_layers):
+        model.add(Conv2D(n_fmap[i], kernel_size=3, padding=padding, activation=activation))
         model.add(Conv2D(n_fmap[i], kernel_size=3, padding=padding, activation=activation))
         model.add(MaxPooling2D())
 
@@ -23,20 +25,26 @@ def build_model(n_conv_layers, n_fmap, input_shape,
     model.add(Dense(n_fmap[-1]))
     model.add(Dropout(dropout))
 
-    model.add(Dense(2, activation='linear'))
+    model.add(Dense(features, activation='linear'))
 
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='mse', optimizer='adam')
 
     return model
 
 
 if __name__ == '__main__':
 
-    model = build_model(3, [16, 32, 64, 64], input_shape=[256, 256, 1])
+    model = build_model(2, 6, [16, 32, 64, 128, 128, 128, 128], input_shape=[256, 256, 1])
     model.summary()
 
     data = np.load(data_path)
 
-    model.fit(x=data['x'], y=data['y'], batch_size=16, epochs=3, validation_split=0.2)
+    model.fit(x=data['x'], y=data['y'], batch_size=16, epochs=25, validation_split=0.2, verbose=2)
 
-    model.save('mean_scale_regressor_0.h5')
+    x = data['x'][0:1]
+    y = model.predict(x)
+
+    print(y)
+    print(data['y'][0:1])
+
+    model.save('models/min_max_regressor_0.h5')